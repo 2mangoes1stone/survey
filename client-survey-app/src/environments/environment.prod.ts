5649e16e8384eedbe6b5ccfaffef9fc1e58cf3b4
Vigi <vigneshnataraj@gmail.com>
2018-11-18 03:31:29 +1100

    edit base url

--- client-survey-app/src/environments/environment.prod.ts
+++ client-survey-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
 
-  baseApiUrl: 'https://survery-server-cefaustzov.now.sh' + '/api'
+  baseApiUrl: 'https://survery-server-ylmdxvkdwl.now.sh ' + '/api'
 };
